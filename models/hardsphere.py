1427443c180fd78f6aba85e6eb5049f1657db561
Jaroslav Fowkes <jaroslav.fowkes@stfc.ac.uk>
2025-12-08 16:38:33 +0000

    Add LUDOX sphere dataset example

---
+++ models/hardsphere.py
@@ -0,0 +1,48 @@
+"""
+SAS Hard Sphere Structure Factor
+https://www.sasview.org/docs/user/models/hardsphere.html
+(adapted from hardsphere.c)
+
+Parameters:
+q - scattering vector
+r_eff - effective radius of hard sphere
+vol_frac - volume fraction of hard sphere
+
+Copyright (C) 2025 The Science and Technology Facilities Council (STFC)
+"""
+import numpy as np
+
+def S_hard_sphere(q, r_eff, vol_frac):
+
+    if(np.abs(r_eff) < 1e-12):
+        return 1.0
+
+    D = (1 / (1 - vol_frac)) ** 2
+    A = ((1 + 2 * vol_frac) * D) ** 2
+    X = np.abs(q * r_eff * 2)
+
+    if(X < 5e-6):
+        return 1/A
+
+    X2 = X * X
+    B = -6 * vol_frac * ((1 + 0.5 * vol_frac) * D) ** 2
+    G = 0.5 * vol_frac * A
+
+    # use Taylor series expansion for small X
+    if (X < 0.05):
+        FF = 8 * A + 6 * B + 4 * G + (
+             -0.8 * A - B / 1.5 - 0.5 * G +
+             (A / 35. + 0.0125 * B + 0.02 * G ) * X2) * X2
+        SF = 1 / (1 + vol_frac * FF)
+        return SF
+
+    X4 = X2 * X2
+    S = np.sin(X)
+    C = np.cos(X)
+
+    FF = ((G * ((4 * X2 - 24) * X * S - (
+           X4 - 12 * X2 + 24) * C + 24) / X2 + B * (
+           2 * X * S - (X2 - 2) * C - 2)) / X + A * (S - X * C)) / X
+    SF = 1 / (1 + 24 * vol_frac * FF / X2)
+
+    return SF
---
+++ sphere_approx_LUDOX.py
@@ -0,0 +1,61 @@
+"""
+Low-rank Sphere Green's function approximation (LUDOX Data)
+https://www.sasview.org/docs/user/models/sphere.html
+
+Parameters:
+q - scattering vector
+r - sphere radius
+drho - difference between scattering length densities
+
+Copyright (C) 2025 The Science and Technology Facilities Council (STFC)
+"""
+import numpy as np
+from models.sphere import G_sphere
+import tt
+
+# for plotting
+import matplotlib.pyplot as plt
+
+# load real SAXS data
+data = np.loadtxt('data/LUDOX/S49_Ludox6_1pct.dat', skiprows=1)
+q = data[:,0]
+nq = len(q)
+
+# contrast
+drho = 1
+
+# r discretisation
+rl = 1
+ru = 10 ** 2.5
+nr = 1000
+r = np.linspace(rl, ru, nr)
+
+# FIXME: forming full G should not be required!
+# form Green's function
+# TODO: check if SASView has any vectorisation
+G = np.zeros((nq,nr))
+for iq in range(nq):
+    for ir in range(nr):
+        G[iq,ir] = G_sphere(q[iq], r[ir], drho)
+
+# FIXME: this is just for plotting
+# plot singular values of G
+s = np.linalg.svd(G, compute_uv=False)
+plt.semilogy(s/s[0])
+plt.grid()
+plt.title("Sphere Green's Function (LUDOX Data)")
+plt.xlabel('Singular Value Index')
+plt.ylabel('Normalised Singular Value')
+plt.savefig('sphere_LUDOX_eigs.png')
+
+# form low-rank TT-representation
+tol = 1e-6
+GTT = tt.tensor(G,tol)
+print(GTT)
+
+# form full-weight array from TT-representation
+fGC = GTT.full()
+E = fGC-G
+error = np.linalg.norm(E)/np.linalg.norm(G)
+print('TT-approximation relative error: ')
+print(error)
