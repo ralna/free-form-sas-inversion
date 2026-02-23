c5746469030fb13ce7c9f1d371ca4965bf2a7970
Jaroslav Fowkes <jaroslav.fowkes@stfc.ac.uk>
2026-02-23 13:23:24 +0000

    Improve SAS inversion script

--- ffsi/optimize.py
+++ ffsi/optimize.py
@@ -112,28 +112,27 @@ def tt_optimize(tt, dims, I_data, I_data_std, check_residual=False, check_deriva
     b2 = mu_over_nv @ one_over_nv
 
     # solve xi0 and b0 using Cramer's rule
-    print("\nCramer's Rule:")
     A = a11 * a22 - a12 * a12
     xi0 = (b1 * a22 - b2 * a12) / A
     b0 = (b2 * a11 - b1 * a12) / A
-    print('xi0:', xi0)
-    print('b0:', b0)
+    print('xi0: %.2e' % xi0)
+    print('b0: %.2e' % b0)
 
     # check residual
     if check_residual:
         x_true_scaled = np.hstack((xi_true/xi0,b_true/b0,w_true))
         eps = np.abs(res(x_true_scaled, xi0=xi0, b0=b0))
-        print('Residual value (min,mean,max): %.2e %.2e %.2e' % (np.min(eps),np.mean(eps),np.max(eps)))
+        print('\nResidual value (min,mean,max): %.2e %.2e %.2e' % (np.min(eps),np.mean(eps),np.max(eps)))
 
     # check derivative
     if check_derivative:
         jac1 = jac(x_true_scaled, xi0=xi0, b0=b0)
         jac2 = approx_derivative(res, x_true_scaled, kwargs={'xi0':xi0,'b0':b0}) # numdiff derivative
         ej = np.abs(jac1-jac2)
-        print('Jacobian difference (min,mean,max): %.2e %.2e %.2e' % (np.min(ej),np.mean(ej),np.max(ej)))
+        print('\nJacobian difference (min,mean,max): %.2e %.2e %.2e' % (np.min(ej),np.mean(ej),np.max(ej)))
 
     # call SciPy least squares with variable scaling
-    print('Calling SciPy least_squares...')
+    print('\nCalling SciPy least_squares...')
     x0_scaled = np.hstack((1,1,w0))
     result = least_squares(res, x0_scaled, jac=jac, bounds=(0,1), verbose=2, kwargs={'xi0':xi0,'b0':b0})
 
@@ -141,7 +140,8 @@ def tt_optimize(tt, dims, I_data, I_data_std, check_residual=False, check_deriva
     xi_opt = result.x[0] * xi0
     b_opt = result.x[1] * b0
     w_opt = result.x[2:]
-    print('xi*:', xi_opt)
-    print('b*:', b_opt)
+    print()
+    print('xi*: %.2e' % xi_opt)
+    print('b*: %.2e' % b_opt)
 
     return xi_opt, b_opt, w_opt
--- ffsi/sas_inversion.py
+++ ffsi/sas_inversion.py
@@ -11,8 +11,8 @@ from ffsi.optimize import tt_optimize
 # for plotting
 import matplotlib.pyplot as plt
 
-## Step 1: Approximate Green's function
-print("Step 1: Approximate Green's function\n")
+## Step 0: Discretisation parameters
+print("Step 0: Discretise parameters\n")
 
 # contrast
 drho = 1
@@ -31,34 +31,11 @@ nr = 500
 q = np.logspace(ql, qu, nq)
 r = np.linspace(rl, ru, nr)
 
-# function for cross-interpolation
-dims = (nq,nr)
-G_func = lambda inds: G_sphere(q[inds[0]], r[inds[1]], drho)
+print('q: logspace(%d,%d,%d)' % (ql, qu, nq))
+print('r: linspace(%d,%d,%d)' % (rl, ru, nr))
 
-# form low-rank TT-representation
-tt = tt_approx(G_func, dims, tol=1e-10, compute_true_error=True)
-
-# FIXME: this is just for plotting the singular values
-print('\nForming G for singular value computation...')
-
-# form Green's function tensor
-G = np.zeros((nq,nr))
-for iq in range(nq):
-    for ir in range(nr):
-        G[iq,ir] = G_sphere(q[iq], r[ir], drho)
-
-# plot singular values of G
-s = np.linalg.svd(G, compute_uv=False)
-plt.figure()
-plt.semilogy(s/s[0])
-plt.grid()
-plt.title("Singular Value Decay")
-plt.xlabel('Singular Value Index')
-plt.ylabel('Normalised Singular Value')
-plt.show()
-
-## Step 2: Generate ground truth
-print("\nStep 2: Generate ground truth\n")
+## Step 1: Generate ground truth
+print("\nStep 1: Generate ground truth\n")
 
 # ground truth radii distributions
 gaussian = np.exp(-(r - 500)**2 / (2*10**2))
@@ -66,7 +43,7 @@ boltzmann = 0.7 * np.exp(-np.abs(r - 700) / 20)
 w_true = gaussian + boltzmann
 w_true /= w_true.sum()  # normalize
 
-# plot ground truth
+# plot ground truth radii distributions
 plt.figure()
 plt.plot(r, w_true)
 plt.grid()
@@ -78,16 +55,22 @@ plt.show()
 # ground truth of scale and background
 scale_true = 2
 b_true = 0.5
-print('b_true:', b_true)
+print('b_true: %.2e' % b_true)
 
 # compute the ground truth of xi
 V = 4/3 * np.pi * r ** 3 # sphere volume
 V_ave = V @ w_true
 xi_true = 1e-4 * scale_true / V_ave
-print('xi_true:', xi_true)
+print('xi_true: %.2e' % xi_true)
 
-# compute intensities
-I_data = xi_true * G @ w_true + b_true
+# compute Gw_true for simulating the intensities
+Gw_true = np.zeros(nq)
+for iq in range(nq):
+    for ir in range(nr):
+        Gw_true[iq] += G_sphere(q[iq], r[ir], drho) * w_true[ir]
+
+# compute model intensities as the intensity data
+I_data = xi_true * Gw_true + b_true
 
 # add a 20%~30% error bar
 np.random.seed(0)
@@ -102,6 +85,34 @@ plt.yscale('log')
 plt.title('Intensity')
 plt.xlabel(r"Scattering vector $q$ ($\AA^{-1}$)")
 plt.ylabel(r"Intensity $I$ ($\mathrm{cm}^{-1}$)")
+plt.show()
+
+## Step 2: Approximate Green's function
+print("\nStep 2: Approximate Green's function\n")
+
+# function for cross-interpolation
+dims = (nq,nr)
+G_func = lambda inds: G_sphere(q[inds[0]], r[inds[1]], drho)
+
+# form low-rank TT-representation
+tt = tt_approx(G_func, dims, tol=1e-10, compute_true_error=True)
+
+# FIXME: this is just for plotting the singular values
+print('\nForming G for singular value computation...')
+# form Green's function tensor
+G = np.zeros((nq,nr))
+for iq in range(nq):
+    for ir in range(nr):
+        G[iq,ir] = G_sphere(q[iq], r[ir], drho)
+
+# plot singular values of G
+s = np.linalg.svd(G, compute_uv=False)
+plt.figure()
+plt.semilogy(s/s[0])
+plt.grid()
+plt.title("Singular Value Decay")
+plt.xlabel('Singular Value Index')
+plt.ylabel('Normalised Singular Value')
 plt.show()
 
 ## Step 3: SAS Inversion with low-rank G
