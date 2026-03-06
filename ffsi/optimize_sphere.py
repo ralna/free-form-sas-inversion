Fix calculation of xi0

ffsi/optimize_sphere.py
@@ -1,5 +1,5 @@
 """
-Free-form SAS Optimization Interface
+Free-form SAS Optimization Interface (Sphere version)
 
 Mandatory Parameters:
 tt - xfac tensor train
@@ -108,11 +108,11 @@ def tt_optimize(tt, dims, I_data, I_data_std,
     mu_over_nv = I_data / I_data_std
     one_over_nv = 1 / I_data_std
     G_ave_over_nv = G_ave / I_data_std
-    a11 = G_ave_over_nv @ G_ave_over_nv
-    a12 = G_ave_over_nv @ one_over_nv
-    a22 = one_over_nv @ one_over_nv
-    b1 = mu_over_nv @ G_ave_over_nv
-    b2 = mu_over_nv @ one_over_nv
+    a11 = np.sum(G_ave_over_nv ** 2)
+    a12 = np.sum(G_ave_over_nv * one_over_nv)
+    a22 = np.sum(one_over_nv ** 2)
+    b1 = np.sum(mu_over_nv * G_ave_over_nv)
+    b2 = np.sum(mu_over_nv * one_over_nv)
 
     # solve xi0 and b0 using Cramer's rule
     A = a11 * a22 - a12 * a12

ffsi/sas_inversion_sphere.py
@@ -1,12 +1,12 @@
 """
-Free-form SAS Inversion Script for Simulated Data
+Free-form SAS Inversion Script for Simulated Sphere Data
 
 Copyright (C) 2026 The Science and Technology Facilities Council (STFC)
 """
 import numpy as np
 from ffsi.models.sphere import G_sphere
 from ffsi.tensor_train import tt_approx
-from ffsi.optimize import tt_optimize
+from ffsi.optimize_sphere import tt_optimize
 
 # for plotting
 import matplotlib.pyplot as plt
@@ -64,6 +64,7 @@ xi_true = 1e-4 * scale_true / V_ave
 print('xi_true: %.2e' % xi_true)
 
 # compute Gw_true for simulating the intensities
+print('\nComputing Gw for simulating the intensities...')
 Gw_true = np.zeros(nq)
 for iq in range(nq):
     for ir in range(nr):

ffsi/sas_inversion_sphere_real_data.py
@@ -1,44 +1,61 @@
 """
-Free-form SAS Inversion Script for Real Data with Structure Factor (LUDOX)
+Free-form SAS Inversion Script for Real Sphere Data (SANS/SAXS)
 
 Copyright (C) 2026 The Science and Technology Facilities Council (STFC)
 """
 import numpy as np
 from ffsi.models.sphere import G_sphere
-from ffsi.models.hardsphere import S_hard_sphere
 from ffsi.tensor_train import tt_approx
-from ffsi.optimize_structure_factor import tt_optimize_structure_factor
+from ffsi.optimize_sphere import tt_optimize
 
 # for plotting
 import matplotlib.pyplot as plt
 
 ## Step 0: Choose dataset
-dataset = 'S49_Ludox6_1pct.dat' # options are in data/LUDOX
+dataset = 'SANS' # options are SANS or SAXS
 
 ## Step 1: Load real data and discretise parameters
 print("Step 1: Load data and discretise parameters\n")
+if dataset == 'SANS':
 
-# load LUDOX data
-data = np.loadtxt('ffsi/data/LUDOX/'+dataset, skiprows=1)
+    # load SANS data
+    data = np.loadtxt('ffsi/data/SANS/observation.txt')
 
-# extract parameters
-tr = 60  # truncate at low-q
-q = data[tr:,0]
-I_data = data[tr:,1]
-I_data_std = data[tr:,2]
-nq = len(q)
+    # extract parameters
+    tr = 285  # truncate at high-q (noisy)
+    q = data[:tr,0]
+    I_data = data[:tr,1]
+    I_data_std = data[:tr,2]
+    nq = len(q)
 
-# r discretisation (log)
-rl = 0
-ru = 2.5
-nr = 1000
-r = np.logspace(rl, ru, nr)
+    # r discretisation
+    rl = 400
+    ru = 800
+    nr = 1000
+    r = np.linspace(rl, ru, nr)
+
+elif dataset == 'SAXS':
+
+    # load SAXS data
+    data = np.loadtxt('ffsi/data/SAXS/observation_corrected.txt')
+
+    # extract parameters
+    q = data[:,0]
+    I_data = data[:,1]
+    I_data_std = data[:,2]
+    nq = len(q)
+
+    # r discretisation
+    rl = 400
+    ru = 1200
+    nr = 1000
+    r = np.linspace(rl, ru, nr)
 
 # contrast
 drho = 1
 
 print('q: fromdata(%d,%d,%d)' % (min(q), max(q), nq))
-print('r: logspace(%d,%d,%d)' % (rl, ru, nr))
+print('r: linspace(%d,%d,%d)' % (rl, ru, nr))
 
 # plot intensities
 plt.figure()
@@ -51,29 +68,8 @@ plt.xlabel(r"Scattering vector $q$ ($\AA^{-1}$)")
 plt.ylabel(r"Intensity $I$ ($\mathrm{cm}^{-1}$)")
 plt.show()
 
-## Step 2: Form structure factor
-print("\nStep 2: Forming Structure Factor\n")
-
-# function for structure factor
-S_func = lambda iq, r_eff, vol_frac: S_hard_sphere(q[iq], r_eff, vol_frac)
-
-# FIXME: this is just for plotting the structure factor
-print('\nForming S for plotting...')
-S = np.zeros(nq)
-for iq in range(nq):
-    S[iq] = S_hard_sphere(q[iq], r_eff=240, vol_frac=0.07)
-
-# plot structure factor
-plt.figure()
-plt.plot(q, S)
-plt.grid()
-plt.xscale('log')
-plt.xlabel(r"Scattering vector $q$ ($\AA^{-1}$)")
-plt.ylabel(r"Structure factor $S$")
-plt.show()
-
-## Step 3: Approximate Green's function
-print("\nStep 3: Approximate Green's function\n")
+## Step 2: Approximate Green's function
+print("\nStep 2: Approximate Green's function\n")
 
 # function for cross-interpolation
 dims = (nq,nr)
@@ -100,9 +96,9 @@ plt.xlabel('Singular Value Index')
 plt.ylabel('Normalised Singular Value')
 plt.show()
 
-## Step 4: SAS Inversion with low-rank G
-print("\nStep 4: SAS inversion with low-rank G\n")
-xi_opt, b_opt, r_eff_opt, vol_frac_opt, w_opt = tt_optimize_structure_factor(tt, dims, I_data, I_data_std, S_func, r_eff_0=180, vol_frac_0=0.05)
+## Step 3: SAS Inversion with low-rank G
+print("\nStep 3: SAS inversion with low-rank G\n")
+xi_opt, b_opt, w_opt = tt_optimize(tt, dims, I_data, I_data_std)
 
 # plot optimized distributions
 plt.figure()