b54364fde66541b3aab57bf609368fa930e1e64e
Jaroslav Fowkes <jaroslav.fowkes@stfc.ac.uk>
2026-06-19 19:34:09 +0000

    Refactor optimize function

---
+++ ffsi/utils.py
@@ -0,0 +1,31 @@
+"""
+Tensor multiplication utility functions
+
+Copyright (C) 2026 The Science and Technology Facilities Council (STFC)
+Author: Jaroslav Fowkes (STFC)
+"""
+import cupy as cp
+
+def contract_tensor(G, w_list, skip_axes):
+    """
+    Contracts G with a list of 1D weight vectors keeping skipped dimensions.
+    """
+    # use CPU or GPU as appropriate
+    xp = cp.get_array_module(G, w_list)
+
+    # create label string for G (e.g. 'abcd')
+    g_labels = [chr(97+i) for i in range(G.ndim)]
+    g_str = "".join(g_labels)
+
+    # identify contracted dimensions
+    w_labels = [l for i,l in enumerate(g_labels) if i not in skip_axes]
+    w_str = ",".join(w_labels)
+
+    # identify output dimensions
+    out_labels = [l for i,l in enumerate(g_labels) if i in skip_axes]
+    out_str = "".join(out_labels)
+
+    # construct einsum formula (e.g. 'abcd,cd->ab')
+    subscripts = f"{g_str},{w_str}->{out_str}"
+
+    return xp.einsum(subscripts, G, *w_list)
